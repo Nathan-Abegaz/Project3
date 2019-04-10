99e5d7fd19958c4e258538eb75f7dffcbf5633ee
Nathan <Nathan@Nathans-MacBook-Pro-2.local>
2019-04-10 11:56:03 -0700

    Update Maze.java
    Completed breakWalls() method

--- Maze.java
+++ Maze.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+import java.util.Stack;
 
 public class Maze {
 	
@@ -16,6 +18,46 @@ public class Maze {
 	
 	public void breakWalls()
 	{
-		
+		Stack<Cell> cellStack = new Stack<>();
+		int totalCells = cells.length;
+		Cell currentCell =  cells[0];
+		int visitedCells = 0;
+		while(visitedCells < totalCells)
+		{
+			if(currentCell.checkWalls())
+			{
+				Cell randomCell = getRandomCell(currentCell);
+				deleteAndConnect(currentCell, randomCell);
+				cellStack.push(currentCell);
+				currentCell = randomCell;
+				visitedCells++;	
+			}
+			else
+			{
+				currentCell = cellStack.pop();
+			}
+		}
+	}
+	
+	/*
+	 * Helper method that retrieved a random cell 
+	 * from a the list of cells with walls intact
+	 * @param cell
+	 */
+	public Cell getRandomCell(Cell cell)
+	{
+		Random randomGenerator = new Random();
+		int index = randomGenerator.nextInt(cell.walls.size());
+		Cell randomCell = cell.walls.get(index);
+		return randomCell;
+	}
+	
+	/*
+	 * 
+	 */
+	public void deleteAndConnect(Cell cell1, Cell cell2)
+	{
+		cell1.dnc(cell2);
+		cell2.dnc(cell1);
 	}
 }
