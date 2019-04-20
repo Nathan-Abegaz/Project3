417699f5cc86607e2914cf28a30d7df443cd7ffc
Nathan-Abegaz <33362510+Nathan-Abegaz@users.noreply.github.com>
2019-04-19 23:07:24 -0700

    Update Cell.java

--- Cell.java
+++ Cell.java
@@ -1,56 +1,60 @@
 package cs146S19.Group.project3;
 
+/*
+ * The cell class would be use to create the maze
+ * Each cell would have a list of all walls and edges
+ * with each cell would keep track whether the cell was discovered
+ * and the index.
+ * 
+ * @author Huynh & Nathan
+ */
 import java.util.ArrayList;
 
 public class Cell {
-	
+
 	ArrayList<Integer> walls = new ArrayList();
 	ArrayList<Integer> edges = new ArrayList();
 	public boolean discovered = false;
 	public int index;
 	public int vis;
-	
+
 	public Cell(int index) {
-		this.index=index;
+		this.index = index;
 	}
-	
+
 	/*
 	 * Adds cell to the list of cells with walls
+	 * 
+	 * @param i,adding index of cell to the list of walls
 	 */
-	public void add(int i)
-	{
+	public void add(int i) {
 		walls.add(i);
-	//	System.out.println(i+ " added to " + this.index + "'s walls");
 	}
-	
-	
+
 	/*
 	 * Checks if the cell has neighboring walls
-	 * @return true/false 
+	 * 
+	 * @return true/false
 	 */
-	public boolean checkWalls()
-	{
-		if(walls.size() > 0)
-		{
+	public boolean checkWalls() {
+		if (walls.size() > 0) {
 			return true;
-		}
-		else
-		{
+		} else {
 			return false;
 		}
 	}
-	
+
 	/*
-	 * Delete cell from list of of walls and 
-	 * add it to the list of edges/connections
+	 * Delete cell from list of walls and add it to the list of
+	 * edges/connections
+	 * 
 	 * @param newCell
 	 */
-	
-	public void dnc(Cell newCell)
-	{
+
+	public void dnc(Cell newCell) {
 		walls.remove(walls.indexOf(newCell.index));
 		edges.add(newCell.index);
-		discovered=true;
+		discovered = true;
 	}
 
 }
