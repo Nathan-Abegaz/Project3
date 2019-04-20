436140070cc6a3e0f142ea0a624670f55b2b788b
Nathan-Abegaz <33362510+Nathan-Abegaz@users.noreply.github.com>
2019-04-19 23:07:02 -0700

    Update BFS.java

--- BFS.java
+++ BFS.java
@@ -6,78 +6,78 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 
-public class BFS
-{
+/*
+ * BFS class implements the Breadth-First Search Algorithm.
+ * The class keep tracks of all the vertices by first initializing all vertices to 
+ * white. Afterwards the algorithm searches through the cells by discovering all of the 
+ * cell's edges before moving on to the next cell. Once a cell has been discovered 
+ * the cell turns grey. After all of the cell's edges have been discovered, the cell turns 
+ * black. The algorithm utilizes a queue to store all 
+ * the discovered cells. The time complexity of this algorithm is O(V + E)
+ * 
+ * @author Huynh & Nathan
+ */
+public class BFS {
 
-   private static final int WHITE = 0;
-   private static final int GREY  = 1;
-   private static final int BLACK = 2;
+	private static final int WHITE = 0;
+	private static final int GREY = 1;
+	private static final int BLACK = 2;
 
-   private Maze maze;
+	private Maze maze;
 
-   int distance = 0;
+	int distance = 0;
 
-   Queue<Integer>     queue;
-   ArrayList<Integer> answerList = new ArrayList<Integer>();
+	Queue<Integer> queue;
+	ArrayList<Integer> answerList = new ArrayList<Integer>();
 
-   BFS(Maze maze)
-   {
-      this.maze = maze;
-      this.queue = new LinkedList<Integer>();
-   }
+	BFS(Maze maze) {
+		this.maze = maze;
+		this.queue = new LinkedList<Integer>();
+	}
 
-   public int[] solve()
-   {
+	/*
+	 * This method solves the maze by utilizing the BFS algorithm
+	 */
+	public int[] solve() {
 
-      //// each v will be v.color=WHITE
-      for (int i = 0; i < maze.cells.length; i++)
-      {
-         maze.cells[i].vis = WHITE;
-      }
-      // ENQUEUE(Q,s);
-      queue.add(maze.cells[0].index);
+		//// each v will be v.color=WHITE
+		for (int i = 0; i < maze.cells.length; i++) {
+			maze.cells[i].vis = WHITE;
+		}
+		// ENQUEUE(Q,s);
+		queue.add(maze.cells[0].index);
 
-      // while (Q not empty)
-      while (!queue.isEmpty())
-      {
+		// while (Q not empty)
+		while (!queue.isEmpty()) {
 
-         // u = DEQUEUE(Q);
-         int index = queue.remove();
-         Cell u = maze.cells[index];
+			// u = DEQUEUE(Q);
+			int index = queue.remove();
+			Cell u = maze.cells[index];
 
-         // for each v ∈ G.adj[u]
-         for (int i = 0; i < u.edges.size(); i++)
-         {
-            Cell v = maze.cells[u.edges.get(i)];
-            if (v.vis == WHITE)
-            {
-               // v.color = GREY;
-               v.vis = GREY;
+			// for each v ∈ G.adj[u]
+			for (int i = 0; i < u.edges.size(); i++) {
+				Cell v = maze.cells[u.edges.get(i)];
+				if (v.vis == WHITE) {
+					// v.color = GREY;
+					v.vis = GREY;
 
-               // v.π = u;
-               // Adding predecessor to and answerList
-               // answerList.add(u.index);
+					// Enqueue(Q, v);
+					queue.add(u.edges.get(i));
 
-               // Enqueue(Q, v);
-               queue.add(u.edges.get(i));
-
-            }
-         }
-         // u.color = BLACK;
-         u.vis = BLACK;
-         //System.out.println(u.index + " Has been fully visited");
-         answerList.add(u.index);
-         if (u.index == (maze.r * maze.r) - 1)
-         {
-            int[] listArray = new int[answerList.size()];
-            for (int i = 0; i < listArray.length; i++)
-            {
-               listArray[i] = answerList.get(i);
-            }
-            return listArray;
-         }
-      }
-      return null;
-   }
+				}
+			}
+			// u.color = BLACK;
+			u.vis = BLACK;
+			answerList.add(u.index);
+			if (u.index == (maze.r * maze.r) - 1) {
+				int[] listArray = new int[answerList.size()];
+				for (int i = 0; i < listArray.length; i++) {
+					listArray[i] = answerList.get(i);
+				}
+				return listArray;
+			}
+		}
+		return null;
+	}
 
 }
